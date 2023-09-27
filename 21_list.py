03d0ea43fb37c467f3e73af669a80b93070e3f52
Bagus Oktaviadi <bagusoktaviadi1@gmail.com>
2023-09-27 11:01:42 +0700

    Update 21_list.py

--- 21_list.py
+++ 21_list.py
@@ -1,6 +1,5 @@
 # list
 variable_list = [1, 2, 3, 4, 5, "5"]
-
 print(variable_list)
 
 
@@ -14,4 +13,13 @@ print(variable_list)
 
 # for loop di kuadrat
 variable_list = [i**2 for i in range(0, 10)]
+print(variable_list)
+
+
+# # for if
+variable_list = [i for i in range(0, 10) if i != 5]
+print(variable_list)
+
+# for if | genap
+variable_list = [i for i in range(0, 10) if i % 2 == 0]
 print(variable_list)
